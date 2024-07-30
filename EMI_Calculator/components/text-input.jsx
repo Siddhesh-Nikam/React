1207a9ca2cabbc72eddb71e9d57e46acad6579dc
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-07-31 01:00:00 +0530

    Add files via upload

---
+++ EMI_Calculator/components/text-input.jsx
@@ -0,0 +1,23 @@
+
+import React from "react";
+
+function TextInput({title,state,setState}){
+    return(
+        <>
+            <span className="title">{title}</span>
+
+            <input
+                type="number"
+                value={state}
+                onChange={(e)=>setState(e.target.value)}
+                placeholder={title}
+            />
+        </>
+    );
+}
+
+export default TextInput;
+
+
+
+
